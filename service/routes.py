ffe3cc5ed29c57f54bf9870bf175867323918810
Albert Khamatshin <akhamatshin@gmail.com>
2022-12-21 20:47:58 +0300

    added delete route

--- service/routes.py
+++ service/routes.py
@@ -93,7 +93,7 @@ def get_accounts(account_id):
     account = Account.find(account_id)
     # abort() with a status.HTTP_404_NOT_FOUND if it cannot be found
     if not account:
-            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] can not be found")
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] can not be found")
     # return the serialize() version of the account with a return code of status.HTTP_200_OK
     return account.serialize(), status.HTTP_200_OK
 
@@ -113,7 +113,7 @@ def update_accounts(account_id):
     account = Account.find(account_id)
     # abort() with a status.HTTP_404_NOT_FOUND if it cannot be found
     if not account:
-            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] can not be found")
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] can not be found")
     # call the deserialize() method on the account passing in request.get_json()
     account.deserialize(request.get_json())
     # call account.update() to update the account with the new data
@@ -127,7 +127,20 @@ def update_accounts(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 
-# ... place you code here to DELETE an account ...
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_accounts(account_id):
+    """
+    Delete an Account
+    This endpoint will delete an Account based on the account_id that is requested
+    """
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+    # use the Account.find() method to retrieve the account by the account_id
+    account = Account.find(account_id)
+    # if found, call the delete() method on the account
+    if account:
+        account.delete()
+    # return and empty body ("") with a return code of status.HTTP_204_NO_CONTENT
+        return "", status.HTTP_204_NO_CONTENT
 
 
 ######################################################################
